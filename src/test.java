82f41646a1f02ba7bf651b774496d14a6c032620
xieweichao <xieweichao@meituan.com>
2018-01-18 17:19:52 +0800

    误差计算修改

--- src/test.java
+++ src/test.java
@@ -9,6 +9,7 @@ import java.util.Vector;
  */
 public class test {
     //计算方差
+    /*
     public static double caculVariance(Vector<Point> undeal, Vector<Point> dealt) {
         Vector<Double> resultSet = new Vector<>();//计算均值方差
         for (int i = 0; i < undeal.size(); i++) {
@@ -27,7 +28,20 @@ public class test {
         double stdev = Math.sqrt(accum / (resultSet.size() - 1)); //方差
         return stdev;
     }
-
+    */
+    public static double caculVariance(Vector<Point> undeal, Vector<Point> dealt) {
+        Vector<Double> resultSet = new Vector<>();//计算均值方差
+        for (int i = 0; i < undeal.size(); i++) {
+            double temp = undeal.get(i).y - dealt.get(i).y;
+            resultSet.add(temp);
+        }
+        double sum = 0.0;
+        for (Double p : resultSet) {
+            sum += p*p;
+        }
+        double stdev = Math.sqrt(sum/resultSet.size()); //方差
+        return stdev;
+    }
     public static void sdfCal(double m_acc) {
         int i = 0;
         Vector<Point> undeal = new Vector<>(), compress = new Vector<>(), dealt = new Vector<>();
@@ -70,21 +84,21 @@ public class test {
         int i = 0;
         Vector<Point> undeal = new Vector<>(), compress = new Vector<>(), dealt = new Vector<>();
         double PI = 3.14159265358979323846;
- /* sin(x)
+
         for (double x_val = -2 * PI; x_val < 1024 * PI; ++i, x_val += 0.2) {
             double sin_y = Math.sin(x_val / 4);
             undeal.add(new Point(i, sin_y));
         }
-
+ /* sin(x)
         //y=-100*sinc ( 0.05*pi*x ) +100
         for(double x_val=-2* PI;x_val<1024*PI;x_val+=0.2,++i) {
             double sin_y = -100 * Math.sin(x_val/4) / (x_val/4);
             undeal.add(new Point(i, sin_y));
         }
- */
+
         File file=new File("/Users/weichaoxie/IdeaProjects/Sdt_project/20170701-20171004.xls");
         undeal=GetExcelInfo.readExcel(file);
-
+ */
         int num = undeal.size();
         DecimalFormat df=new DecimalFormat("####0.000000");
         long startTime=System.currentTimeMillis();
@@ -111,7 +125,7 @@ public class test {
     public static void main(String[] args) {
 
         double m_acc=0.01;
-        double m_accArray[]={5,1,0.8,0.5,0.3,0.1,0.05,0.03,0.01};
+        double m_accArray[]={1,0.8,0.5,0.3,0.1,0.05,0.03,0.01};
         for(int i=0;i<m_accArray.length;i++){
             System.out.println("m_acc:"+m_accArray[i]);
             m_acc=m_accArray[i];
